cdbed9b2e2a88b378410cec34a495dbc4609abaa
Tony Cheng <tcheng102@gmail.com>
2020-01-18 19:27:55 -0600

    added experiments on delayed board evaluation and reward functions.

---
+++ multi_teacher-fixed_all-advice_all/model.py
@@ -0,0 +1,186 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.utils as utils
+import torch.nn.functional as F
+import torch.optim as optim
+from random import random
+
+
+def conv2d_size_out(size, kernel_size=5, stride=2):
+    return (size - (kernel_size - 1) - 1) // stride + 1
+
+
+class DQN(nn.Module):
+    def __init__(self, height, width, channel_in, action_dim, input_size, device):
+        super(DQN, self).__init__()
+
+        self.device = device
+
+        # Vision processing
+        self.conv1 = nn.Conv2d(channel_in, 32, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+
+        self.convw = conv2d_size_out(conv2d_size_out(
+            conv2d_size_out(width, 3, 1), 3, 2), 3, 1)
+        self.convh = conv2d_size_out(conv2d_size_out(
+            conv2d_size_out(height, 3, 1), 3, 2), 3, 1)
+
+        # Language processing
+        self.lstm_hidden_size = 128
+        self.embedding = nn.Embedding(input_size, 32)
+        self.lstm = nn.LSTM(32, self.lstm_hidden_size)
+
+        # Gated-Attention layers
+        self.attn_linear = nn.Linear(self.lstm_hidden_size, 64)
+
+        self.l1 = nn.Linear(self.convw * self.convh * 64, 256)
+
+        self.l_action = nn.Linear(256, action_dim)
+
+        self.to(self.device)
+
+    def forward(self, inputs):
+        state, advice = inputs
+
+        # Image processing
+        x = F.relu(self.conv1(state))
+        x = F.relu(self.conv2(x))
+        image_rep = F.relu(self.conv3(x))
+
+        # Language processing
+        word_embedding = []
+        for i in range(len(advice)):
+            word_embedding.append(self.embedding(advice[i]))
+        word_embedding = utils.rnn.pad_sequence(word_embedding)
+
+        encoder_hidden = (torch.zeros(1, len(advice), self.lstm_hidden_size, requires_grad=True).to(self.device),
+                          torch.zeros(1, len(advice), self.lstm_hidden_size, requires_grad=True).to(self.device))
+
+        _, encoder_hidden = self.lstm(word_embedding, encoder_hidden)
+        advice_rep = encoder_hidden[0].view(len(advice), -1)
+
+        # Attention
+        advice_attention = torch.sigmoid(self.attn_linear(advice_rep))
+
+        # Gated-Attention
+        advice_attention = advice_attention.unsqueeze(2).unsqueeze(3)
+        advice_attention.expand(advice_attention.size(0),
+                                64, self.convh, self.convw)
+
+        x = image_rep * advice_attention
+        x = x.view(x.size(0), -1)
+
+        x = F.relu(self.l1(x))
+        action_values = self.l_action(x)
+
+        return action_values
+
+
+class Model(object):
+    def __init__(self, lr, height, width, channel_in, action_dim, input_size=128, writer=None, device=None):
+        if device is None:
+            self.device = torch.device(
+                "cuda" if torch.cuda.is_available() else "cpu")
+        else:
+            self.device = device
+
+        self.writer = writer
+
+        self.input_size = input_size
+
+        self.dqn1 = DQN(height, width, channel_in, action_dim,
+                        self.input_size, self.device)
+        self.dqn2 = DQN(height, width, channel_in,
+                        action_dim, self.input_size, self.device)
+
+        self.dqn1_optimizer = optim.Adam(self.dqn1.parameters(), lr=lr)
+        self.dqn2_optimizer = optim.Adam(self.dqn2.parameters(), lr=lr)
+
+        self.words = {}
+        self.word_counter = 0
+
+    def add_word(self, word):
+        if word not in self.words:
+            self.word_counter += 1
+            self.words[word] = self.word_counter
+
+    def advice_to_idx(self, advice):
+        advice_idxes = []
+        for i in range(len(advice)):
+            advice_idx = torch.zeros(
+                len(advice[i]), dtype=torch.long).to(self.device)
+            for j in range(len(advice[i])):
+                self.add_word(advice[i][j])
+                advice_idx[j] = self.words[advice[i][j]]
+            advice_idxes.append(advice_idx)
+        return advice_idxes
+
+    def select_action(self, state, advice, dqn_num, epsilon=0.05):
+        val = random()
+        if val < epsilon:
+            return int(random() * 4)
+        else:
+            with torch.no_grad():
+                state = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+                advice = self.advice_to_idx([advice])
+                if dqn_num == 0:
+                    action = self.dqn1((state, advice))
+                else:
+                    action = self.dqn2((state, advice))
+                return np.argmax(action.cpu().data.numpy().flatten())
+
+    def update(self, bs, replay_buffer, dqn_num, gamma=0.9):
+        if len(replay_buffer) < bs:
+            return
+        (states, advices), actions, rewards, next_states, dones = replay_buffer.sample(bs)
+        states = torch.FloatTensor(states).to(self.device)
+        advices = self.advice_to_idx(advices)
+        rewards = torch.FloatTensor(rewards).to(self.device)
+        next_states = torch.FloatTensor(next_states).to(self.device)
+        dones = torch.FloatTensor(dones).to(self.device)
+        actions = np.stack((np.arange(bs), actions))
+        if dqn_num == 0:
+            q_values = self.dqn1((states, advices))[actions]
+            next_q_values = torch.max(self.dqn2((next_states, advices)), 1)[0]
+        else:
+            q_values = self.dqn2((states, advices))[actions]
+            next_q_values = torch.max(self.dqn1((next_states, advices)), 1)[0]
+        
+
+        expected_q_values = rewards + (gamma * next_q_values * (1 - dones)).detach()
+
+        loss = F.smooth_l1_loss(q_values, expected_q_values)
+
+        if dqn_num == 0:
+            self.dqn1_optimizer.zero_grad()
+            loss.backward()
+            self.dqn1_optimizer.step()
+        else:
+            self.dqn2_optimizer.zero_grad()
+            loss.backward()
+            self.dqn2_optimizer.step()
+
+        if self.writer is not None:
+            self.writer.add_histogram(
+                'q_values', q_values.detach())
+
+        return loss
+
+    def save(self, directory, name):
+        torch.save(self.dqn.state_dict(), '%s/%s_dqn.pth' % (directory, name))
+        torch.save(self.dqn_target.state_dict(),
+                   '%s/%s_dqn_target.pth' % (directory, name))
+        torch.save(self.words, '%s/%s_words.pth' % (directory, name))
+        torch.save(self.word_counter, '%s/%s_word_counter.pth' %
+                   (directory, name))
+
+    def load(self, directory, name):
+        self.dqn.load_state_dict(torch.load('%s/%s_dqn.pth' % (directory, name),
+                                            map_location=lambda storage, loc: storage))
+        self.dqn_target.load_state_dict(torch.load('%s/%s_dqn_target.pth' % (directory, name),
+                                                   map_location=lambda storage, loc: storage))
+        self.words = torch.load('%s/%s_words.pth' % (directory, name))
+        self.word_counter = torch.load(
+            '%s/%s_word_counter.pth' % (directory, name))
---
+++ multi_teacher-fixed_all-advice_all/utils.py
@@ -0,0 +1,206 @@
+import numpy as np
+from PIL import Image
+import torch
+import torchvision.transforms as T
+from random import random
+from krazy_gridworld import Color_Index
+
+
+class Goal(object):
+    red_goal = 0
+    blue_goal = 1
+    green_goal = 2
+    red_lava = 3
+    blue_lava = 4
+    green_lava = 5
+    avoid_lava = 6
+    avoid_goal = 7
+
+
+advices = {
+    "Reach red goal",
+    "Reach blue goal",
+    "Reach green goal",
+    "Reach red lava",
+    "Reach blue lava",
+    "Reach green lava",
+    "Avoid any lava",
+    "Avoid any lava"
+}
+
+
+class OptimisticTeacher(object):
+    def __init__(self):
+        super(OptimisticTeacher, self).__init__()
+
+    def get_advice(self, color, at_goal, is_lava):
+        advice_list = []
+
+        for advice in advices:
+            if advice == "Avoid any lava" or advice == "Avoid any lava":
+                if not advice_not_satisfied(advice, color, at_goal, is_lava):
+                    advice_list.append(advice)
+            elif advice_satisfied(advice, color, at_goal, is_lava):
+                advice_list.append(advice)
+
+        if len(advice_list) == 0:
+            return None
+        return advice_list[int(random() * len(advice_list))]
+
+
+class DiscouragingTeacher(object):
+    def __init__(self):
+        super(DiscouragingTeacher, self).__init__()
+
+    def get_advice(self, color, at_goal, is_lava):
+        advice_list = []
+
+        for advice in advices:
+            if advice == "Avoid any lava" or advice == "Avoid any lava":
+                if advice_not_satisfied(advice, color, at_goal, is_lava):
+                    advice_list.append(advice)
+            elif not advice_satisfied(advice, color, at_goal, is_lava):
+                advice_list.append(advice)
+
+        if len(advice_list) == 0:
+            return None
+        return advice_list[int(random() * len(advice_list))]
+
+
+class ReplayBuffer:
+    def __init__(self, max_size=5000):
+        self.teacher1 = OptimisticTeacher()
+        self.teacher2 = DiscouragingTeacher()
+        self.max_size = max_size
+        self.reset()
+
+    def reset(self):
+        self.all_states = []
+        self.all_advices = []
+        self.all_actions = []
+        self.all_rewards = []
+        self.all_next_states = []
+        self.all_dones = []
+
+    def new_episode(self):
+        self.cur_states = []
+        self.cur_actions = []
+        self.cur_next_states = []
+        self.cur_dones = []
+        self.cur_colors = []
+        self.cur_at_goals = []
+        self.cur_is_lavas = []
+
+    def add(self, state, action, next_state, done, color, at_goal, is_lava):
+        self.cur_states.append(state)
+        self.cur_actions.append(action)
+        self.cur_next_states.append(next_state)
+        self.cur_dones.append(done)
+        self.cur_colors.append(color)
+        self.cur_at_goals.append(at_goal)
+        self.cur_is_lavas.append(is_lava)
+
+    def compute_reward(self, color, at_goal, is_lava, gamma=0.99):
+        optimistic_advice = self.teacher1.get_advice(color, at_goal, is_lava)
+        discouraging_advice = self.teacher2.get_advice(color, at_goal, is_lava)
+
+        if optimistic_advice is not None:
+            for i in range(len(self.cur_states)):
+                self.all_states.append(self.cur_states[i])
+                self.all_advices.append(optimistic_advice.split(' '))
+                self.all_actions.append(self.cur_actions[i])
+                if (optimistic_advice == "Avoid any goal" or optimistic_advice == "Avoid any lava") and \
+                        not advice_not_satisfied(optimistic_advice, color, at_goal, is_lava):
+                    self.all_rewards.append(1/25)
+                elif advice_satisfied(optimistic_advice, self.cur_colors[i], self.cur_at_goals[i], self.cur_is_lavas[i]):
+                    self.all_rewards.append(1)
+                else:
+                    self.all_rewards.append(0)
+                self.all_next_states.append(self.cur_next_states[i])
+                self.all_dones.append(self.cur_dones[i])
+
+        if discouraging_advice is not None:
+            for i in range(len(self.cur_states)):
+                self.all_states.append(self.cur_states[i])
+                self.all_advices.append(discouraging_advice.split(' '))
+                self.all_actions.append(self.cur_actions[i])
+                if (discouraging_advice == "Avoid any goal" or discouraging_advice == "Avoid any lava") and \
+                        not advice_not_satisfied(discouraging_advice, color, at_goal, is_lava):
+                    self.all_rewards.append(1/25)
+                elif advice_satisfied(discouraging_advice, self.cur_colors[i], self.cur_at_goals[i], self.cur_is_lavas[i]):
+                    self.all_rewards.append(1)
+                else:
+                    self.all_rewards.append(0)
+                self.all_next_states.append(self.cur_next_states[i])
+                self.all_dones.append(self.cur_dones[i])
+
+        if (len(self.all_states) > self.max_size):
+            self.all_states = self.all_states[-self.max_size:]
+            self.all_advices = self.all_advices[-self.max_size:]
+            self.all_actions = self.all_actions[-self.max_size:]
+            self.all_rewards = self.all_rewards[-self.max_size:]
+            self.all_next_states = self.all_next_states[-self.max_size:]
+            self.all_dones = self.all_dones[-self.max_size:]
+
+    def __len__(self):
+        return len(self.all_states)
+
+    def sample(self, bs=64):
+
+        if len(self.all_states) < bs:
+            return None
+
+        indexes = np.arange(len(self.all_states))
+        np.random.shuffle(indexes)
+        indexes = indexes[:bs]
+        states, advices, actions, rewards, next_states, dones = [], [], [], [], [], []
+
+        for i in indexes:
+            state, advice, action, reward, next_state, done = self.all_states[i], self.all_advices[
+                i], self.all_actions[i], self.all_rewards[i], self.all_next_states[i], self.all_dones[i]
+            states.append(np.array(state, copy=False))
+            advices.append(np.array(advice, copy=False))
+            actions.append(np.array(action, copy=False))
+            rewards.append(np.array(reward, copy=False))
+            next_states.append(np.array(next_state, copy=False))
+            dones.append(np.array(done, copy=False))
+
+        return (np.array(states), np.asarray(advices)), np.array(actions), np.array(rewards), \
+            np.array(next_states), np.array(dones)
+
+
+def sample_advice():
+    advice_list = list(advices)
+    advice = advice_list[int(random() * len(advice_list))]
+    if advice == "Avoid any lava" or advice == "Avoid any goal":
+        return sample_advice()
+    else:
+        return advice
+
+
+def get_state(env):
+    return env.get_combined_obs().transpose((2, 0, 1))
+
+
+def advice_satisfied(initial_advice, color, at_goal, is_lava):
+    if at_goal and color == Color_Index.red and initial_advice == "Reach red goal":
+        return True
+    if at_goal and color == Color_Index.blue and initial_advice == "Reach blue goal":
+        return True
+    if at_goal and color == Color_Index.green and initial_advice == "Reach green goal":
+        return True
+    if is_lava and color == Color_Index.red and initial_advice == "Reach red lava":
+        return True
+    if is_lava and color == Color_Index.blue and initial_advice == "Reach blue lava":
+        return True
+    if is_lava and color == Color_Index.green and initial_advice == "Reach green lava":
+        return True
+    return False
+
+
+def advice_not_satisfied(initial_advice, color, at_goal, is_lava):
+    if at_goal and initial_advice == "Avoid any goal":
+        return True
+    if is_lava and initial_advice == "Avoid any lava":
+        return True
+    return False
